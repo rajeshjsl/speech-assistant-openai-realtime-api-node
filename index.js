f6cddb3a3b067c79cf458ebf2bc1d759fcb11b88
rajeshjsl <35141819+rajeshjsl@users.noreply.github.com>
2024-10-09 23:15:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ fastify.all('/incoming-call', async (request, reply) => {
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
                               <Say>Please wait while we connect your call to the Khona Tea representative.</Say>
-                              <Pause length="1"/>                              
+                              <Pause length="1"/>
+                              <Say>Hello.</Say>
                               <Connect>
                                   <Stream url="wss://${request.headers.host}/media-stream" />
                               </Connect>
